8f515187b59428e16b8001740330118fffd945b5
jeffry <36665036+wangxin688@users.noreply.github.com>
2024-01-19 12:55:15 +0800

    Update init_pg.py

--- deploy/init_pg.py
+++ deploy/init_pg.py
@@ -1,8 +1,16 @@
+import asyncio
+
 from sqlalchemy import text
-from sqlalchemy.ext.asyncio import AsyncSession
 
+from src.db.session import async_session
 
-async def create_pg_extensions(session: AsyncSession) -> None:
-    await session.execute(text('create EXTENSION if not EXISTS "pgcrypto"'))
-    await session.execute(text('create EXTENSION if not EXISTS "hstore"'))
-    await session.commit()
+async def create_pg_extensions() -> None:
+    async with async_session() as session:
+        await session.execute(text('create EXTENSION if not EXISTS "pgcrypto"'))
+        await session.execute(text('create EXTENSION if not EXISTS "hstore"'))
+        await session.commit()
+
+if __name__ == "__main__":
+    asyncio.run(create_pg_extensions())
+    
+
